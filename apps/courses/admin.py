01fbab8611ae20b87a5551b7bfcec2fa395b747d
DAKExDUCK <arsengabdulin228@gmail.com>
2023-01-22 14:26:30 +0600

    add like and save buttons, change admin panel fields for Courses

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -12,11 +12,19 @@ class LessonAdmin(admin.ModelAdmin):
 
 class CourseAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ("slug", "title", "count_of_lessons")
+    list_display = ("slug", "title", "count_of_lessons", "status", "count_of_likes", "count_of_saves")
 
     @admin.display(description='Count of lessons')
-    def count_of_lessons(self, obj):
-        return len(obj.lessons.all())
+    def count_of_lessons(self, obj: Course):
+        return obj.get_count_lessons()
+    
+    @admin.display(description='Count of likes')
+    def count_of_likes(self, obj: Course):
+        return obj.get_count_likes()
+    
+    @admin.display(description='Count of saves')
+    def count_of_saves(self, obj: Course):
+        return obj.get_count_saves()
     
     ordering = ('id',)
 
